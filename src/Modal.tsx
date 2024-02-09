b63d4a508a56b4e6345727524519faed3510a8c8
Gina Alfandary Magana <ginaleealfy@gmail.com>
2024-02-08 18:02:02 -0800

    added typescript and updated modal
    modal doesn't import anything from other parts so it is a good file to start with.

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, MutableRefObject, ReactElement } from "react";
 import { createPortal } from "react-dom";
 
-const Modal = ({ children }) => {
-  const elRef = useRef(null);
+const Modal = ({ children }: { children: ReactElement }) => {
+  const elRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
 
   //don't need to keep making elRef.current div. Helps with performance
   if (!elRef.current) {
@@ -11,10 +11,17 @@ const Modal = ({ children }) => {
 
   useEffect(() => {
     const modalRoot = document.getElementById("modal");
+    if (!modalRoot || !elRef.current) {
+      return;
+    }
     modalRoot.appendChild(elRef.current);
 
     //this happens right before modal is closed.
-    return () => modalRoot.removeChild(elRef.current);
+    return () => {
+      if (elRef.current) {
+        modalRoot.removeChild(elRef.current);
+      }
+    };
   }, []);
 
   return createPortal(<div>{children}</div>, elRef.current);
